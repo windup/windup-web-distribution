1f83b850d92384a6ec6180de6d910487c62bb66b
Marek Novotný <hotmana76@gmail.com>
2017-03-22 16:10:12 +0100

    taiga task 582 - environment property to use different data directory for windup web

--- src/main/resources/run_windup.sh
+++ src/main/resources/run_windup.sh
@@ -2,5 +2,14 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ -z "$WINDUP_DATA_DIR" ] ; then
+    export WINDUP_DATA_DIR=$DIR/standalone/data
+fi
+
+if [ "$WINDUP_DATA_DIR" != "$DIR/standalone/data" ] ; then
+    export JAVA_OPTS="-Dwindup.data.dir=$WINDUP_DATA_DIR"
+    $DIR/bin/jboss-cli.sh --file=update-windup-datasource.cli
+fi
+
 $DIR/bin/standalone.sh -c standalone-full.xml
 
