4fdfd938b83bdfc2c42967ee012ab37901a1178c
Jess Sightler <jesse.sightler@gmail.com>
2017-03-23 18:16:44 -0400

    Changed the approach a bit.
    I found that the previous approach did not behave well when moving from having
    an environmental variable to not having one. While this would be ok
    on a transient instance, it would break a local install.
    
    Instead, this uses a temporary system property to get the data source setup,
    then the property is always passed in to standalone when the application is run.
    
    This seems to work well locally.

--- src/main/resources/run_windup.sh
+++ src/main/resources/run_windup.sh
@@ -6,10 +6,5 @@ if [ -z "$WINDUP_DATA_DIR" ] ; then
     export WINDUP_DATA_DIR=$DIR/standalone/data
 fi
 
-if [ "$WINDUP_DATA_DIR" != "$DIR/standalone/data" ] ; then
-    export JAVA_OPTS="-Dwindup.data.dir=$WINDUP_DATA_DIR"
-    $DIR/bin/jboss-cli.sh --file=update-windup-datasource.cli
-fi
-
-$DIR/bin/standalone.sh -c standalone-full.xml
+$DIR/bin/standalone.sh -c standalone-full.xml -Dwindup.data.dir=$WINDUP_DATA_DIR
 
