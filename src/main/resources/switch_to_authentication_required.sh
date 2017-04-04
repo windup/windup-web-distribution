411f0472fbc6c4abfc24effb1f7d772d9e3cfcf6
Jess Sightler <jesse.sightler@gmail.com>
2017-04-04 13:55:18 -0400

    Rebranded Windup Web Distribution

--- src/main/resources/switch_to_authentication_required.sh
+++ src/main/resources/switch_to_authentication_required.sh
@@ -11,7 +11,7 @@ echo "================================"
 echo ""
 echo "The system will now require an authentication step."
 echo ""
-echo "We recommend that you login to http://localhost:8080/auth and remove the guest user from the Windup realm at this point".
+echo "We recommend that you login to http://localhost:8080/auth and remove the guest user from the RHAMT realm at this point".
 echo "(Default Keycloak user: admin, password: password)"
 echo ""
 echo "================================"
