d2453a7cdf17e34b2da4b98444f750a5af435681
Jess Sightler <jesse.sightler@gmail.com>
2017-04-10 15:10:24 -0400

    Added Dockerfile for local docker builds

--- src/main/resources/run_rhamt.sh
+++ src/main/resources/run_rhamt.sh
@@ -8,4 +8,4 @@ if [ -z "$WINDUP_DATA_DIR" ] ; then
     export WINDUP_DATA_DIR=$DIR/standalone/data
 fi
 
-$DIR/bin/standalone.sh -c standalone-full.xml -Dwindup.data.dir=$WINDUP_DATA_DIR
+$DIR/bin/standalone.sh -c standalone-full.xml -Dwindup.data.dir=$WINDUP_DATA_DIR $@
