aafa2495e921587ff2f33fed16b2bf8cef8609a1
Marek Novotný <hotmana76@gmail.com>
2017-04-25 13:34:36 +0200

    taiga task #781: adding windows scripts

---
+++ src/main/resources/switch_to_automatic_authentication.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set "DIR=%~dp0"
+
+cd "%DIR%"
+
+copy "%DIR%\themes\rhamt\login\auto_login.properties" "%DIR%\themes\rhamt\login\theme.properties"
+
+echo "================================"
+echo ""
+echo "The system won't require an authentication step anymore."
+echo ""
+echo "If you previously removed guest user, login to http://localhost:8080/auth and add user 'rhamt' with password 'password' to the RHAMT realm".
+echo "(Default Keycloak user: admin, password: password)"
+echo ""
+echo "================================"
