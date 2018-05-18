1bb49562f2b7f078f7e985050ea5db1eeb681e9b
Jess Sightler <jesse.sightler@gmail.com>
2018-05-18 16:11:05 -0400

    WINDUP-2012: Update windup-web-distribution to use windup-openshift

--- src/main/resources/openshift/builder/inject.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ${JBOSS_HOME}/standalone/log
-
-# Run our CLI script
-${JBOSS_HOME}/bin/jboss-cli.sh --file=${JBOSS_HOME}/standalone/configuration/eap.cli >>${JBOSS_HOME}/standalone/log/inj.log 2>&1
