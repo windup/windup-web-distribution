3fcc5df9fc06d1f90c95fa12940903942506b253
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2022-04-12 10:19:31 +0200

    [WINDUP-3333] - Web Console fails to start if any of the parent folders contains blank spaces (#83)
    * Avoid the run script to fail when parent folder contains spaces
    
    * Fix windows run script to avoid it failing when parent folder contains spaces
    
    * Fix Windows script

--- src/main/resources/run_mta.sh
+++ src/main/resources/run_mta.sh
@@ -69,7 +69,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export JBOSS_HOME=$DIR
 
 if [ -z "$WINDUP_DATA_DIR" ] ; then
-    export WINDUP_DATA_DIR=$DIR/standalone/data
+    export WINDUP_DATA_DIR="$DIR/standalone/data"
 fi
 
-$DIR/bin/standalone.sh -c standalone-full.xml -Dwindup.data.dir=$WINDUP_DATA_DIR $@
+"$DIR/bin/standalone.sh" -c standalone-full.xml -Dwindup.data.dir="$WINDUP_DATA_DIR" $@
