33c6b89d3d48123b7014285a01c5c80f5ebf3751
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2022-07-14 10:49:17 +0200

    [WINDUP-3363]-MTA - support multiple themes (#84)
    * Cover multiple themes
    
    * Remove unused profiles
    
    * Default user always be "migration"
    
    * Update versions

--- src/main/resources/switch_to_authentication_required.bat
+++ src/main/resources/switch_to_authentication_required.bat
@@ -4,13 +4,13 @@ set "DIR=%~dp0"
 
 cd "%DIR%"
 
-copy "%DIR%\themes\mta\login\login_required.theme.properties" "%DIR%\themes\mta\login\theme.properties"
+copy "%DIR%\themes\${product-name}\login\login_required.theme.properties" "%DIR%\themes\${product-name}\login\theme.properties"
 
 echo "================================"
 echo ""
 echo "The system will now require an authentication step."
 echo ""
-echo "We recommend that you login to http://localhost:8080/auth and remove the default 'mta' user from the MTA realm at this point".
+echo "We recommend that you login to http://localhost:8080/auth and remove the default 'migration' user from the realm at this point".
 echo "(Default Keycloak user: admin, password: password)"
 echo ""
 echo "================================"
